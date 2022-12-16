801843a593fff6259e74b36ea1b776c35ab2f05b
marcuschuang <marcuschuang@gmail.com>
2022-12-16 00:13:46 -0600

    Inital Commit

---
+++ src/main/java/com/marcus/learn/springbootwebappmysql/SpringbootWebappMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.marcus.learn.springbootwebappmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebappMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebappMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcus/learn/springbootwebappmysql/SpringbootWebappMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcus.learn.springbootwebappmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebappMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
